c14e1f4793b8c85f1eb64acdc6901d88a724af49
clintongrove <divekid@gmail.com>
2015-01-10 11:28:17 -0800

    PR2_5

---
+++ PR2_5/src/pr2_5/PI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr2_5;
+
+/**
+ *
+ * @author hgrove17
+ */
+public class PI {
+    static double generatepi(double c){
+    double j=3;
+    double pi=4;
+    for (int x=0;x<c;x++){
+    
+    pi+=(-(4/j)+(4/(j+2)));    
+    j+=4;   
+    
+}return pi;
+}
+}
